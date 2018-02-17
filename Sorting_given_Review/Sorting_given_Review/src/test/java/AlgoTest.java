db10671196ee70e1582caf84287882816c620314
Travis Hawley <tlhawley@asu.edu>
2018-02-16 19:42:22 -0700

    Added Test Cases from previous work(modified) to cover all Algos

--- Sorting_given_Review/Sorting_given_Review/src/test/java/AlgoTest.java
+++ Sorting_given_Review/Sorting_given_Review/src/test/java/AlgoTest.java
@@ -10,13 +10,14 @@ import main.java.SortAlgos;
 import main.java.SortDemoData;
 
 class AlgoTest {
-
+	
+	SortDemoData data = new SortDemoData();
+	
     /**
      * sameSize checks that the given array and the output array have the same size
      */
     @Test
     public void sameSize() {
-        SortDemoData data = new SortDemoData();
         data.initializeArray("2 3 4");
         Item[] copyOfMyArray = new Item[data.myArray.length];
         System.arraycopy(
@@ -54,5 +55,205 @@ class AlgoTest {
         assertTrue(array[1].key == 4);
         assertTrue(array[2].key == 5);
     }
+    
+    /**
+     * Node Coverage-Merge
+     */
+  	@Test
+  	public void NodeMergeTest() {
+  		data.initializeArray("101 100 64 72 31");  
+
+  		data.runAlgo(3);
+  		for(int i = 0; i < 3; i++)
+  		{
+  			for(int j = i+1; j < 5; j++)
+  			{
+  				assertTrue(data.myArray[i].key < data.myArray[j].key);
+  			}
+  		}
+  	}
+
+  	/**
+  	 * Edge Coverage-Merge
+  	 */
+  	@Test
+  	public void EdgeMergeTest() {
+  		data.initializeArray("1");
+
+  		data.runAlgo(3);
+  		assertTrue(data.myArray[0].key == 1);
+  	}
+
+
+  	/**
+  	 * Condition Coverage-Merge
+  	 */
+  	@Test
+  	public void ConditionMergeTest() {
+  		assertThrows(NullPointerException.class,
+                ()->{
+                	data.runAlgo(3);
+                });
+  	}
+
+  	/**
+  	 * Node Coverage-Selection
+  	 */
+  	@Test
+  	public void NodeSelectionTest() {
+  		data.initializeArray("101 100 64 72 31");  
+
+  		data.runAlgo(1);
+  		for(int i = 0; i < 3; i++)
+  		{
+  			for(int j = i+1; j < 5; j++)
+  			{
+  				assertTrue(data.myArray[i].key < data.myArray[j].key);
+  			}
+  		}
+  	}
+
+  	/**
+  	 * Edge Coverage-Selection
+  	 */
+  	@Test
+  	public void EdgeSelectionTest() {
+  		data.initializeArray("1");
+
+  		data.runAlgo(1);
+  		assertTrue(data.myArray[0].key == 1);
+  	}
+
+
+  	/**
+  	 * Condition Coverage-Selection
+  	 */
+  	@Test
+  	public void ConditionSelectionTest() {
+  		assertThrows(NullPointerException.class,
+                ()->{
+                	data.runAlgo(1);
+                });
+  	}
+
+  	/**
+  	 * Node Coverage-Insertion
+  	 */
+  	@Test
+  	public void NodeInsertionTest() {
+  		data.initializeArray("101 100 64 72 31");  
+
+  		data.runAlgo(2);
+  		for(int i = 0; i < 3; i++)
+  		{
+  			for(int j = i+1; j < 5; j++)
+  			{
+  				assertTrue(data.myArray[i].key < data.myArray[j].key);
+  			}
+  		}
+  	}
+
+  	/**
+  	 * Edge Coverage-Insertion
+  	 */
+  	@Test
+  	public void EdgeInsertionTest() {
+  		data.initializeArray("1");
+
+  		data.runAlgo(2);
+  		assertTrue(data.myArray[0].key == 1);
+  	}
+
+
+  	/**
+  	 * Condition Coverage-Insertion
+  	 */
+  	@Test
+  	public void ConditionInsertionTest() {
+  		assertThrows(NullPointerException.class,
+                ()->{
+                	data.runAlgo(2);
+                });
+  	}
+
+  	/**
+  	 * Node Coverage-Quick
+  	 */
+  	@Test
+  	public void NodeQuickTest() {
+  		data.initializeArray("101 100 64 72 31");  
+
+  		data.runAlgo(4);
+  		for(int i = 0; i < 3; i++)
+  		{
+  			for(int j = i+1; j < 5; j++)
+  			{
+  				assertTrue(data.myArray[i].key < data.myArray[j].key);
+  			}
+  		}
+  	}
+
+  	/**
+  	 * Edge Coverage-Quick
+  	 */
+  	@Test
+  	public void EdgeQuickTest() {
+  		data.initializeArray("1");
+
+  		data.runAlgo(4);
+  		assertTrue(data.myArray[0].key == 1);
+  	}
+
+
+  	/**
+  	 * Condition Coverage-Quick
+  	 */
+  	@Test
+  	public void ConditionQuickTest() {
+  		assertThrows(NullPointerException.class,
+                ()->{
+                	data.runAlgo(4);
+                });
+  	}
+
+  	/**
+  	 * Node Coverage-Heap
+  	 */
+  	@Test
+  	public void NodeHeapTest() {
+  		data.initializeArray("101 100 64 72 31");  
+
+  		data.runAlgo(5);
+  		for(int i = 0; i < 3; i++)
+  		{
+  			for(int j = i+1; j < 5; j++)
+  			{
+  				assertTrue(data.myArray[i].key < data.myArray[j].key);
+  			}
+  		}
+  	}
+
+  	/**
+  	 * Edge Coverage-Heap
+  	 */
+  	@Test
+  	public void EdgeHeapTest() {
+  		data.initializeArray("1");
+
+  		data.runAlgo(5);
+  		assertTrue(data.myArray[0].key == 1);
+  	}
+
+
+  	/**
+  	 * Condition Coverage-Heap
+  	 */
+  	@Test
+  	public void ConditionHeapTest() {
+  		assertThrows(NullPointerException.class,
+                ()->{
+                	data.runAlgo(5);
+                });
+  	}
 }
 
